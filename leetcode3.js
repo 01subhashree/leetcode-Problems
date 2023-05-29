a8af346512f658897ffab870df8b91a048344cb6
01subhashree <ssubhashree094@gmail.com>
2023-05-29 20:39:11 +0530

    save file

--- leetcode3.js
+++ leetcode3.js
@@ -1,22 +1,24 @@
 function isValid(s) {
-    const stack = []
+  const stack = [];
 
-    for (i=0; i < s.length; i++){
+  for (i = 0; i < s.length; i++) {
+    let curChar = s[i];
 
-        let curChar = s[i];
-
-        switch(curChar) {
-            case '(': stack.push(')');
-                break;
-            case '[': stack.push(']');
-                break;
-            case '{': stack.push('}')
-                break;
-            default:
-                topElement = stack.pop()
-                if (curChar !== topElement) return false;       
-        }
+    switch (curChar) {
+      case "(":
+        stack.push(")");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      case "{":
+        stack.push("}");
+        break;
+      default:
+        topElement = stack.pop();
+        if (curChar !== topElement) return false;
     }
-    return stack.length === 0;
-};
-console.log(isValid("()[]{}"))
+  }
+  return stack.length === 0;
+}
+console.log(isValid("()[]{}"));
--- leetcood2.js
+++ leetcood2.js
@@ -1,16 +1,16 @@
 function longestCommonPrefix(strs) {
-    let prefix = ""
-   if(strs === null || strs.length === 0) return prefix
+  let prefix = "";
+  if (strs === null || strs.length === 0) return prefix;
 
-   for (let i=0; i < strs[0].length; i++){ 
-       const char = strs[0][i] 
+  for (let i = 0; i < strs[0].length; i++) {
+    const char = strs[0][i];
 
-       for (let j = 1; j < strs.length; j++){ 
-           if(strs[j][i] !== char) return prefix
-       }
-       prefix = prefix + char
-   }
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== char) return prefix;
+    }
+    prefix = prefix + char;
+  }
 
-   return prefix
-};
-console.log(longestCommonPrefix(["flower","flow","flight"]))
+  return prefix;
+}
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
